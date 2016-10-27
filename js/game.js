7e02ba7c35d57cb20568cfe6369299b7649f11bc
Carver Harrison <pcarverb@hotmail.com>
2016-10-27 13:00:08 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,9 +2,7 @@ jQuery(function($, undefined) {
     var curdir = "";
 
     $('#terminal').terminal(function(command, term) {
-        var code = [];
-        code = command.split(['.', ',', '#', '?', ':', '"', '(', ')']);
-        console.log(code);
+        term.echo(eval("vm." + command.split(';')[0]));
     }, {
         greetings: 'Welcome to the CanviOS Alpha!',
         name: 'terminal',
