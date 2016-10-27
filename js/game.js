30b47eeaa687bcb8cfbf92ea712e485af5e0c105
Carver Harrison <pcarverb@hotmail.com>
2016-10-27 11:45:35 -0700

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,21 @@
+jQuery(function($, undefined) {
+    $('#terminal').terminal(function(command, term) {
+        if (command !== '') {
+            try {
+                var result = window.eval(command);
+                if (result !== undefined) {
+                    term.echo(new String(result));
+                }
+            } catch(e) {
+                term.error(new String(e));
+            }
+        } else {
+           term.echo('');
+        }
+    }, {
+        greetings: 'Welcome to the CanviOS Alpha!',
+        name: 'terminal',
+        height: 200,
+        prompt: '> '
+    });
+});
